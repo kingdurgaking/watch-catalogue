3adc1f7eaeef3cd7a3e6f970bc8aedd67440cb79
durga <kingchand007@yahoo.com>
2020-08-18 15:15:10 +0400

    Initial Project set up of watch catalogue

---
+++ src/main/java/com/maha/catalogue/WatchCatalogueApplication.java
@@ -0,0 +1,13 @@
+package com.maha.catalogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WatchCatalogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchCatalogueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maha/catalogue/WatchCatalogueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maha.catalogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WatchCatalogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
